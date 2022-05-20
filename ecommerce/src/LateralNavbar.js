9ef1dbbaf268d118e21f92937e92e2ba097131e9
Angelo Pavan <angelopavan.dev@gmail.com>
2022-05-20 19:59:36 +0200

    Aggiunto un pulsante sulla navbar superiore

--- ecommerce/src/LateralNavbar.js
+++ ecommerce/src/LateralNavbar.js
@@ -2,9 +2,26 @@ import './assets/css/LateralNavbar.css';
 
 function LateralNavbar() {
   return (
-    <div className="row">
-      <h3 className="koulen-font mt-3">Categorie</h3>
+    <div>
+      <div className="row">
+        <h2 className="koulen-font my-3">Categorie</h2>
+      </div>
+
+      <div className="ms-4">
+        <div className="row">
+          <h5 className="my-2">Capelli</h5>
+        </div>
+
+        <div className="row">
+          <h5 className="my-2">Corpo</h5>
+        </div>
+
+        <div className="row">
+          <h5 className="my-2">Mani</h5>
+        </div>
+      </div>   
     </div>
+
   );
 }
 
